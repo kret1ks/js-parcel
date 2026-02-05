87c99337302f314ca0fc284bfdcd3270cb43248a
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-02-05 10:40:52 +0200

    123

--- src/opp.js
+++ src/opp.js
@@ -1,11 +1,8 @@
 import { calculator, subtract, multiply, divide} from "./js/calculator"
 
 console.log("додавання:", calculator(10, 5));
+console.log("віднімання:", subtract(100, 5));
 console.log("множення:", multiply(10, 5));
 console.log("ділення:", divide(10, 2));
 
 
-const title = document.querySelector('h1');
-if (title) {
-    title.textContent = `Результат додавання: ${add(10, 20)}`;
-}
