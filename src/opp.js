3ea984b0ba90002bc824b1207556165f284a2412
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-02-05 10:53:51 +0200

    fix

--- src/opp.js
+++ src/opp.js
@@ -1,4 +1,4 @@
-import { calculator, subtract, multiply, divide} from "./js/calculator"
+import { calculator, subtract, multiply, divide} from "./js/calculator.js"
 
 console.log("додавання:", calculator(10, 5));
 console.log("віднімання:", subtract(100, 5));
